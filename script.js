88bb3e6b6cc67a176034e2fd27c20ea2f167aed3
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-20 12:12:43 +0300

    logical-operators

---
+++ script.js
@@ -0,0 +1,32 @@
+// logical opertaors
+// 1. logical opertaors AND(&&)
+// 2. logical opertaors OR (||)
+// 3. logical opertaors NOT (!)
+
+//  1. AND(&&)
+
+let a = true;
+let b = false;
+let c = 4;
+console.log(c > 2 && c < 2);
+console.log(c < 2 && c < 2);
+
+//  OR (||)
+
+console.log(c < 2 || c < 2);
+console.log(c > 2 || c < 2);
+
+let isweekend = true;
+let isholiday = false;
+if (isweekend || isholiday){
+    console.log('you can relax');
+}
+
+// NOT (!)
+
+let isRaining = false;
+if(!isRaining){
+    console.log('go outside');
+}
+
+// Arrays
