a72e6bd8395a05423c4b7672d7f597a5b4adb157
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-21 12:28:20 +0300

    arrays

--- script.js
+++ script.js
@@ -30,3 +30,58 @@ if(!isRaining){
 }
 
 // Arrays
+
+// array litterals []
+let list = [4,5,7,8];
+list[0] = 'yes'
+console.log(list)
+
+// push= adds un array at the end 
+
+const fruits = [
+    "panana",
+    "mango",
+    "pineapple",
+    "orange"
+]
+console.log(fruits)
+fruits.push()
+
+// pop= removes un array at the end
+
+fruits.pop()
+
+// shift removes an array at the start
+fruits.shift()
+// ushift = add Array at the begening
+fruits.unshift()
+
+// join
+let people =["Ahmed","ibrahim", "Noor" ]
+console.log(people.join('-'))
+
+// reverse
+console.log(people.reverse())
+
+// slice
+console.log(people.slice())
+
+
+
+
+// opject
+let info = {
+    course: 'java',
+    location: ["kenya", "Garissa" ],
+    level: 'all levels',
+    school: 'zein empower',
+    id: '445522',
+    duration: "7weeks",
+    chapters: {
+        section1: 'basic',
+        section2: 'intermediate',
+        section3: 'advance'
+    },
+}
+console.log(info)
+console.log(info.course = "python");
